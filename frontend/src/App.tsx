808f86631327e958809c377e2e7976c28eba062a
krishna singh <0.krishna1120@gmail.com>
2026-05-11 12:51:26 +0530

    react routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 
+import { Signup } from "./screens/Signup";
+import { Signin } from "./screens/Signin";
+import { VideoPage } from "./screens/VideoPage";
+import { LandingPage } from "./screens/LandingPage";
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/watch" element={<VideoPage />} />
         <Route path="/" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
 
 export default App;
---
+++ frontend/src/screens/VideoPage.tsx
@@ -0,0 +1,7 @@
+export function VideoPage() {
+    return (
+        <div>
+            <h1>VideoPage</h1>
+        </div>
+    );
+}
