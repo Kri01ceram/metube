98de1fb963e6d2d6b63e0fe19ef5e9e96ba8ec45
krishna singh <0.krishna1120@gmail.com>
2026-05-15 14:21:55 +0530

    videos lao screen prr

--- frontend/src/screens/LandingPage.tsx
+++ frontend/src/screens/LandingPage.tsx
@@ -1,5 +1,16 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export function LandingPage() {
-    
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/videos")
+            .then(response => {
+                const data = response.data;
+            });
+    }, []);
+
     return (
         <div>
             <h1>Landing Page</h1>
