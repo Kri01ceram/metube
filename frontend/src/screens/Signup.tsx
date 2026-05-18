3ebf1fd8960be4840dc11eed8e6fc43b9b3bcb3a
krishna singh <0.krishna1120@gmail.com>
2026-05-18 10:10:41 +0530

    signup hogya connect bhi hogya

--- frontend/src/screens/Signup.tsx
+++ frontend/src/screens/Signup.tsx
@@ -1,7 +1,25 @@
+import axios from "axios";
+
 export function Signup() {
+   async function signup() {
+        axios.post("http://localhost:3000/api/signup", {
+            username: document.getElementById("username")?.value,
+            password: document.getElementById("password")?.value,
+            channelName: document.getElementById("channelName")?.value,
+            gender: "Male"
+        }).then(response => {
+            const data = response.data;
+            localStorage.setItem("token", data.token);
+            window.location = "/signin";
+        });
+       
+}
     return (
         <div>
-            <h1>Signup</h1>
+            <input id="username" type="text" placeholder="Username" />
+            <input id="password" type="text" placeholder="Password" />
+            <input id="channelName" type="text" placeholder="ChannelName" />
+            <button onClick={(signup)}>Sign up</button>
         </div>
     );
 }
