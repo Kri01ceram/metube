bed92f273ec4b91fd3f5d1c53d42d8d9125fb04b
krishna singh <0.krishna1120@gmail.com>
2026-05-18 09:49:17 +0530

    upload page

--- frontend/src/screens/UploadPage.tsx
+++ frontend/src/screens/UploadPage.tsx
@@ -1,7 +1,21 @@
+import axios from "axios";
+
 export function UploadPage() {
+    function upload(){
+        axios.post("http://localhost:3000/api/videos", {
+              videoUrl: document.getElementById("videoUrl")?.value,
+              thumbnail: document.getElementById("thumbnail")?.value
+        },{
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        })
+    }
     return (
         <div>
-            Upload Page
+            <input id="videoUrl" type="text" placeholder="Video URL" />
+            <input id="thumbnail" type="text" placeholder="Thumbnail" />
+            <button onClick={(upload)}>Complete upload</button>
         </div>
     );
 }
