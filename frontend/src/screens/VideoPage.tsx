92ead681eb867dddfd67ac33c565c2247f08815f
krishna singh <0.krishna1120@gmail.com>
2026-06-26 11:31:09 +0530

    feat: implement loading state and fetch recommended videos in VideoPage component

--- frontend/src/screens/VideoPage.tsx
+++ frontend/src/screens/VideoPage.tsx
@@ -5,16 +5,31 @@ import { useSearchParams } from "react-router";
 export function VideoPage() {
     const [seachParams,setSearchParams] = useSearchParams();
     const [videoDetails,setVideoDetails] = useState();
+    const [isLoading,setIsLoading] = useState(true);
+    const [recommendedVideos,setRecommendedVideos] = useState([]);
     const id = seachParams.get("id");
     useEffect(()=>{
         axios.get(`http://localhost:3000/api/videos/${id}`)
             .then(response => {
                 const data = response.data;
                 setVideoDetails(data);
+                setIsLoading(false);
             });
     },[id])
+    useEffect(()=>{
+        axios.get("http://localhost:3000/api/videos")
+            .then(response => {
+                const data = response.data;
+                setRecommendedVideos(data);
+            });
+    },[])
+
+    if(isLoading){
+        return <div>Loading...</div>
+    }
     return (
-        <div>
+        <div style={{display:"flex"}}>
+            <div>
             <video src={videoDetails?.videoUrl} />
             <br />
             <div>
@@ -28,5 +43,8 @@ export function VideoPage() {
             </div>
            
         </div>
+        <div
+        </div>
+        
     );
 }
